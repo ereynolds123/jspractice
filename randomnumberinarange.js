f849cf0d5d9b8c36d2314fb1e31077660ab435ce
ereynolds123 <51254660+ereynolds123@users.noreply.github.com>
2020-05-05 14:51:32 -0400

    Create randomnumberinarange.js

---
+++ randomnumberinarange.js
@@ -0,0 +1,5 @@
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+  return Math.floor(Math.random() * (myMax- myMin+1))+myMin; //This returns a random whole integer between the range.
+  // Only change code above this line
+}
